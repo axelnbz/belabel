b78781956e2c81015bc2a855e6db174d8b578356
axelnbz <156028684+axelnbz@users.noreply.github.com>
2024-01-10 21:04:13 -0800

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "ed402fc9c425eb94907ff5b4cd161ebd",
-"index.html": "3de92fda9bf3f43f90d713fa4bdabb73",
-"/": "3de92fda9bf3f43f90d713fa4bdabb73",
-"main.dart.js": "1051db2c973a15a90a1359d6c9e26f5c",
+"index.html": "f6ccb8188ad7236a402974e5c42f30f1",
+"/": "f6ccb8188ad7236a402974e5c42f30f1",
+"main.dart.js": "aa44f9f0ed7d6e3af462757c0074d0c2",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
